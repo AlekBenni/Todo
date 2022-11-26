e968d627078b8236345daa6bd765a91c5e5c0212
AlekBenni <adeyva@mail.ru>
2022-11-26 22:00:26 +0400

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { v1 } from 'uuid';
 import Todolist, { TaskType } from './Todolist';
 
 export type FilterValuesType = "all" | "complited" | "active";
@@ -6,17 +7,23 @@ export type FilterValuesType = "all" | "complited" | "active";
 function App() {
 
 let [tasks, setTasks] = useState<Array<TaskType>>([
-  {id: 1, title: "CSS" , isDone: true},
-  {id: 2, title: "JS" , isDone: true},
-  {id: 3, title: "React" , isDone: false},
-  {id: 4, title: "Redax" , isDone: false}
+  {id: v1(), title: "CSS" , isDone: true},
+  {id: v1(), title: "JS" , isDone: true},
+  {id: v1(), title: "React" , isDone: false},
+  {id: v1(), title: "Redax" , isDone: false}
 ])
 
 let [filter, setFilter] = useState<FilterValuesType>("all");
 
-function removeTask(id:number){
+function removeTask(id:string){
   let filteredTasks = tasks.filter(t => t.id !== id)
   setTasks(filteredTasks)
+}
+
+function addTask(task:string){
+  let newTask = {id: v1(), title: task, isDone: false}
+  let newTasks = [newTask, ...tasks];
+  setTasks(newTasks)
 }
 
 function changeFilter(fil:FilterValuesType){
@@ -40,6 +47,7 @@ if(filter === "active"){
           tasks={tasksForTodolist} 
           removeTask = {removeTask}
           changeFilter = {changeFilter}
+          addTask = {addTask}
           />
         </div>
       </div>      
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,46 +1,73 @@
+import { ChangeEvent, KeyboardEvent, useState } from "react";
 import { FilterValuesType } from "./App";
 
 type TodolistPropsType = {
     title: string;
     tasks: Array<TaskType>;
-    removeTask: (id: number) => void;
+    removeTask: (id: string) => void;
     changeFilter: (value: FilterValuesType) => void;
+    addTask: (task:string) => void
 }
 
 export type TaskType = {
-  id: number;
+  id: string;
   title: string;
   isDone: boolean;
 }
 
 function Todolist (props:TodolistPropsType){
+  let [newTaskTitle, setNewTaskTitle] = useState("");
+  const onChangeTitleHandler = (e:ChangeEvent<HTMLInputElement>) => {
+    setNewTaskTitle(e.currentTarget.value)
+  }
+  const onKeyPressHandler = (e:KeyboardEvent<HTMLInputElement>) => {
+    if(e.keyCode === 13){
+      props.addTask(newTaskTitle)
+      setNewTaskTitle("")}}
+  const addTask = () => {
+    props.addTask(newTaskTitle)
+    setNewTaskTitle("")
+  }    
+  const onAllClickHandler = () => {props.changeFilter("all")}
+  const onAlctiveClickHandler = () => {props.changeFilter("active")}
+  const onComplitedClickHandler = () => {props.changeFilter("complited")}
+  
     return (
       <div className='col-4'>
         <div className='border p-3 rounded shadow '>
         <h3>{props.title}</h3>
         <div>
         <div className="input-group mb-3">
-          <input type="text" className="form-control" placeholder="Text me" aria-label="Recipient's username" aria-describedby="button-addon2"/>
-          <button className="btn btn-danger" type="button" id="button-addon2">Button</button>
+          <input type="text" 
+          onKeyDown={(e) => { onKeyPressHandler(e) }}  
+          value={newTaskTitle} 
+          onChange={(e) => {onChangeTitleHandler(e)}}
+          className="form-control" placeholder="Add a new Task" aria-label="Recipient's username" aria-describedby="button-addon2"/>
+          <button onClick={() => {addTask()}}
+          className="btn btn-danger" type="button" id="button-addon2">Button</button>
         </div>
         <ul className='list-group'>
           {
           props.tasks.map((task) => {
+            const onRemoveHandler = () => {
+              props.removeTask(task.id)
+            }
               return (
-                <li className="list-group-item d-flex justify-content-between align-items-center">
+                <li key={task.id} className="list-group-item d-flex justify-content-between align-items-center">
                   <div>
                   <input className="form-check-input me-2" type="checkbox" checked={task.isDone} id="flexCheckDefault"/>
                   <span>{task.title}</span>
                   </div>
-                  <button type="button" className="btn btn-outline-danger btn-sm" onClick={() => {props.removeTask(task.id)}}>X</button>
+                  <button type="button" className="btn btn-outline-danger btn-sm" 
+                  onClick={() => {onRemoveHandler()}}>X</button>
                 </li>
               )
           })}
         </ul>
         <div className='d-flex justify-content-around mt-3'>
-          <button onClick={() => props.changeFilter("all")} type="button" className="btn btn-primary">All</button>
-          <button onClick={() => props.changeFilter("active")} type="button" className="btn btn-success">Active</button>
-          <button onClick={() => props.changeFilter("complited")} type="button" className="btn btn-danger">Complited</button>
+          <button onClick={() => onAllClickHandler()} type="button" className="btn btn-primary">All</button>
+          <button onClick={() => onAlctiveClickHandler()} type="button" className="btn btn-success">Active</button>
+          <button onClick={() => onComplitedClickHandler()} type="button" className="btn btn-danger">Complited</button>
         </div>
         </div>
         </div>
